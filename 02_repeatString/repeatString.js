4070ca39b692174cf63ae953f7f7420be6fa0c94
sysfield <139412420+sysfield@users.noreply.github.com>
2023-09-19 21:45:08 +0100

    Add conditional for negative numbers

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
 const repeatString = function(word, times) {
     var output = [];
+    if (times < 0)
+    {
+        return 'ERROR';
+    }
     for (var i = 0; i < times; i++)
     {
         output.push(word);

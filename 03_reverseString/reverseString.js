0a1d7e15ca051ffac56adb03a26505aa0c884d4d
sysfield <139412420+sysfield@users.noreply.github.com>
2023-09-19 21:54:41 +0100

    Add function to attempt reversal

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    var newString = [];
+    for (var i = string.length; i > 0; i--)
+    {
+        newString.unshift(string[i]);
+    }
+    return newString;
 };
 
 // Do not edit below this line

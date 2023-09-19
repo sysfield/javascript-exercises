5566620b56d93a5467053c0dc6621b276ce765ca
sysfield <139412420+sysfield@users.noreply.github.com>
2023-09-19 21:39:42 +0100

    Add function to pass first test Create array for function to pass into on for loop. Return join array, with no separator

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(word, times) {
+    var output = [];
+    for (var i = 0; i < times; i++)
+    {
+        output.push(word);
+    }
+    return output.join('');
 };
 
 // Do not edit below this line

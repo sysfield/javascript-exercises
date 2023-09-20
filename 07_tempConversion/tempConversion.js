0d122cfbb18aab159ec9bee34bccd6935379a751
sysfield <139412420+sysfield@users.noreply.github.com>
2023-09-20 20:45:00 +0100

    Add initial logic

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  return fahrenheit = Number.parseFloat(fahrenheit * (9/5) + 32).toFixed(1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  return celsius = Number.parseFloat((celsius - 32) * (5/9)).toFixed(1);
 };
 
 // Do not edit below this line

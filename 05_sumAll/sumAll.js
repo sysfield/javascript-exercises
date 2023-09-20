9261991d6f3197bfaa88e9e7e65f227e0951a3c3
sysfield <139412420+sysfield@users.noreply.github.com>
2023-09-20 20:15:41 +0100

    First iteration of loop

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,10 @@
-const sumAll = function() {
-
+const sumAll = function(first, final) {
+    var sum = 0;
+    for (var i = first; i < final + 1; i++)
+    {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line

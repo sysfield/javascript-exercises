4216359f6b3698a648ed1fb79e46f5e9a2c77a56
sysfield <139412420+sysfield@users.noreply.github.com>
2023-09-20 20:37:18 +0100

    First iteration of conditional

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,16 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if (!(year % 100))
+    {
+        if (!(year % 400))
+        {
+            return true;
+        }
+    }
+    else if (!(year % 4))
+    {
+        return true;
+    }
+    return false;
 };
 
 // Do not edit below this line

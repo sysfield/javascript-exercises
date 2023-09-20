0507127d6efbd33d42f5d7c23cfb4fb225264872
sysfield <139412420+sysfield@users.noreply.github.com>
2023-09-20 21:01:56 +0100

    Factor first iteration of function

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
 const convertToCelsius = function(fahrenheit) {
-  return fahrenheit = Number.parseFloat(fahrenheit * (9/5) + 32).toFixed(1);
+  if (Number.parseInt((fahrenheit - 32) * (5/9)).toFixed(1) == 0.0)
+  {
+    return 0;
+  }
+  return Number.parseInt((fahrenheit - 32) * (5/9)).toFixed(1);
 };
 
 const convertToFahrenheit = function(celsius) {
-  return celsius = Number.parseFloat((celsius - 32) * (5/9)).toFixed(1);
+  return Number.parseFloat((celsius * (9/5)) + 32).toFixed(1);
 };
 
 // Do not edit below this line
